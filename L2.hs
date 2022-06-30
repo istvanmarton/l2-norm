78b89b026cec59e9115f8cafbb6af560a98a8b09
Péter Diviánszky <divip@divip.hu>
2022-06-30 14:52:27 +0200

    implement parallel execution

--- L2.hs
+++ L2.hs
@@ -1,11 +1,18 @@
 -- optimized version of L2.hs
 --
 -- Optimizations:
---  - vector norms are cached (~ 2x speedup)
---  - not all submatrix norms are calculated (~ 2x speedup)
+--  - vector norms are cached
+--  - not all submatrix norms are calculated
+--  - parallel execution
 
-import qualified Data.Vector.Unboxed as V
 import System.Environment
+import qualified Data.Vector.Unboxed as V
+import Control.Monad
+import Control.Monad.Par.IO
+import Control.Monad.IO.Class
+import Control.Monad.Par.Class
+import Data.IORef
+import Control.Concurrent
 
 type Vec = V.Vector Int
 
@@ -24,6 +31,7 @@ withNorm1 v = (v, norm1 v)
 
 type L2Norm = Int
 
+type Mat = [Vec]
 type MatWithNorms = [(L2Norm, Vec)]
 
 f :: VecWithNorm -> VecWithNorm -> MatWithNorms -> L2Norm -> L2Norm
@@ -33,21 +41,43 @@ f (va, na) (vb, nb) m i = case m of
     | i >= n + na + nb -> i
     | otherwise -> f (withNorm1(v .+. va)) (vb, nb) m' (f (va, na) (withNorm1(v .+. vb)) m' i)
 
-f' v m = f (withNorm1(V.replicate (V.length v) 0)) (withNorm1 v) m 0
+fPar :: IORef L2Norm -> VecWithNorm -> VecWithNorm -> MatWithNorms -> ParIO L2Norm
+fPar best (va, na) (vb, nb) m = case m of
+  (n, v): m' | n == maxBound -> do
+    c1 <- spawn_ (fPar best (withNorm1 $ v .+. va) (vb, nb) m')
+    c2 <- spawn_ (fPar best (va, na) (withNorm1 $ v .+. vb) m')
+    liftM2 max (get c1) (get c2)
+  _ -> do
+    i <- liftIO $ readIORef best
+    let i' = f (va, na) (vb, nb) m i
+    i' `seq` liftIO (atomicModifyIORef best (\i -> (max i' i, ())))
+    pure i'
 
-g [] = []
-g (v: m) = (f' v mn, v): mn  where
-  mn = g m
+{-# INLINE f' #-}
+f' :: L2Norm -> Vec -> MatWithNorms -> IO L2Norm
+f' guess v vs = do
+  best <- newIORef guess
+  runParIO $ fPar best (withNorm1 $ V.replicate (V.length v) 0) (withNorm1 v) vs
 
-conc m1 m2@((n, _): _) = map (\(a, b) -> (a + n, b)) m1 ++ m2
+g :: Int -> Mat -> IO MatWithNorms
+g _ [] = pure []
+g l (v: vs) = do
+  m <- g (l-1) vs
+  let m' = zipWith (\i (a, b) -> (if i < (length vs + 1) `div` 4 then maxBound else a, b)) [0..] m
+  i <- if l > 0 then pure maxBound else f' 0 v m'
+  pure ((i, v): m)
 
-l2 :: [Vec] -> L2Norm
-l2 (v: m) = f' v (g m1 `conc` g m2)
-  where
-    (m1, m2) = splitAt (length m `div` 2) m
+l2 :: Int -> Mat -> IO L2Norm
+l2 guess (v:vs) = do
+  m <- g ((length vs + 1) `div` 4) vs
+  f' guess v m
 
 main :: IO ()
 main = do
-    [f] <- getArgs
-    s <- readFile f
-    print . l2 . map (V.fromList . map read . words) . filter (not . null) $ lines s
+  args <- getArgs
+  case args of
+    [guess, f] -> do
+      s <- readFile f
+      let m = (map (V.fromList . map read . words) . takeWhile (not . null) . lines) s
+      i <- l2 (read guess) m
+      print i
